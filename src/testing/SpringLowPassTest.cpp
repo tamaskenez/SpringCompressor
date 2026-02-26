4d333c2455168e1db8f74bd16f5fa349017db398
Tamas Kenez <tamas.kenez@gmail.com>
2026-02-26 11:56:34 +0100

    SpringCompressor cosine test

--- src/testing/SpringLowPassTest.cpp
+++ src/testing/SpringLowPassTest.cpp
@@ -2,11 +2,30 @@
 
 #include "SpringLowPass.h"
 
+#include <cmath>
 #include <fstream>
+#include <numbers>
 
 TEST(SpringLowPass, always_pass)
 {
     SUCCEED();
+}
+
+TEST(SpringLowPass, save_cosine_response)
+{
+    constexpr double sample_rate = 1000.0;
+    constexpr int num_samples = static_cast<int>(sample_rate); // 1 second
+
+    SpringLowPass filter(sample_rate);
+    filter.set_critically_damped_with_time_constant(0.1);
+
+    std::ofstream file(TESTING_OUTPUT_DIR "/SpringLowPass_cosine.txt");
+    ASSERT_TRUE(file.is_open());
+
+    for (int i = 0; i < num_samples; ++i) {
+        const double input = std::cos(2.0 * std::numbers::pi * i / 3.0);
+        file << filter.process(input) << "\n";
+    }
 }
 
 TEST(SpringLowPass, save_IR)
