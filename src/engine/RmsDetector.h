123826448833f695a0e6ea6b364a6470e9932cf5
Tamas Kenez <tamas.kenez@gmail.com>
2026-03-13 21:26:31 +0100

    Remove unused filter bank rms detector

--- src/engine/RmsDetector.h
+++ src/engine/RmsDetector.h
@@ -27,35 +27,3 @@ private:
     double mean_square = 0.0;
     variant<double, Biquad_TDF2> filter = 0.0;
 };
-
-struct FilterBankRmsDetectorConfig {
-    struct FilterBank {
-        int half_order;
-        double freq_lo_hz;
-        double freq_hi_hz;
-        double filters_per_octave;
-        double Q;
-    } filter_bank;
-    struct LPF {
-        int order;
-        double K;
-        optional<double> attack_time_constant; // When it's set, order is ignored, SpringLowPass will be used.
-    } lpf;
-};
-
-class FilterBankRmsDetector
-{
-public:
-    FilterBankRmsDetector(double fs, const FilterBankRmsDetectorConfig& config_arg);
-    void process(span<double> samples);
-
-private:
-    FilterBankRmsDetectorConfig config;
-    FilterBankBandPass2 fb;
-    struct LP {
-        std::vector<FirstOrderIIR_TDF2> first;
-        std::vector<Biquad_TDF2> second;
-        std::vector<SpringLowPass> spring;
-    } lp;
-    vector<double> filter_bank_output_samples;
-};
