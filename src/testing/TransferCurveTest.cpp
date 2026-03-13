f31ccf7972cb5617806161c05ebe4768ed5075ae
Tamas Kenez <tamas.kenez@gmail.com>
2026-03-13 21:21:56 +0100

    Update TransferCurveTest

--- src/testing/TransferCurveTest.cpp
+++ src/testing/TransferCurveTest.cpp
@@ -17,49 +17,41 @@ bool equal_decimals(X x, Y y, int num_decimals)
 
 TEST(TransferCurve, T1)
 {
-    GTEST_SKIP(); // TODO: Enable after refactoring the makeup gain - reference level mechanism.
+    GTEST_SKIP(); // Manual smoke test with printing only.
 #if 0
-    TransferCurve tc;
-    TransferCurvePars tcp;
-    std::println("A=[");
-    for (int k = 0; k < 4; ++k) {
-        switch (k) {
-        case 0:
-        case 1:
-            break;
-        case 2:
-            tcp.knee_width_db = 10;
-            tc.set(tcp);
-            break;
-        case 3:
-            tcp.normalizer_db = 5;
-            tc.set(tcp);
-            break;
-        }
-        for (double i = tcp.threshold_db - 10; i <= 0; ++i) {
+        TransferCurve tc;
+        TransferCurvePars tcp;
+        std::println("A=[");
+        for (int k = 0; k < 4; ++k) {
             switch (k) {
             case 0:
-                std::print("{} ", i);
-                break;
             case 1:
-            case 2:
-            case 3:
-                std::print("{} ", i + tc.gain_db_for_input_db(i));
                 break;
-            default:
-                assert(false);
+            case 2:
+                tcp.knee_width_db = 10;
+                tc.set(tcp);
+                break;
+            case 3:
+                tcp.makeup_gain_db = 5;
+                tc.set(tcp);
+                break;
             }
+            for (double i = tcp.threshold_db - 10; i <= 0; ++i) {
+                switch (k) {
+                case 0:
+                    std::print("{} ", i);
+                    break;
+                case 1:
+                case 2:
+                case 3:
+                    std::print("{} ", i + tc.gain_db_for_input_db(i));
+                    break;
+                default:
+                    assert(false);
+                }
+            }
+            std::println();
         }
-        std::println();
-    }
-    std::println("]';");
-    std::println("Slowly raising makeup gain");
-    std::println("|makeupgain|reference_level|out@ref");
-    for (float mg = -5; mg <= 5; mg += 0.1f) {
-        mg = round(mg * 10) / 10;
-        tcp.normalizer_db = mg;
-        auto r = tc.set(tcp);
-        std::println("{} {} {}\n", mg, r.reference_level_db, tc.gain_db_for_input_db(r.reference_level_db));
-    }
+        std::println("]';");
 #endif
 }
